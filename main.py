3877bafaf722c920c70869ab2390542484796281
KripaMishra <kripa5661@gmail.com>
2023-10-18 02:13:18 +0530

    Model Training Complete

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlProject import logger
 from src.mlProject.pipeline.Stage_01_data_ingestion import  DataIngestionTrainingPipeline
 from src.mlProject.pipeline.Stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlProject.pipeline.Stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.mlProject.pipeline.Stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -31,6 +32,18 @@ try:
    data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
