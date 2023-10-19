f982078e024983e90a5e0f45e5b1c6a2bde4b336
KripaMishra <kripa5661@gmail.com>
2023-10-19 13:42:19 +0530

    ml flow experiments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.mlProject.pipeline.Stage_01_data_ingestion import  DataIngestionTrainin
 from src.mlProject.pipeline.Stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlProject.pipeline.Stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.mlProject.pipeline.Stage_04_model_trainer import ModelTrainerTrainingPipeline
+from src.mlProject.pipeline.Stage_05_Model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -42,6 +43,16 @@ STAGE_NAME = "Model Trainer stage"
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
